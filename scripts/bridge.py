4b7c056103e62714c86516e9b4c4f91817358f9b
Alphakyl <kyle.harlow@colorado.edu>
2021-04-28 18:34:53 -0600

    Updated topic names and drop ranges

--- scripts/bridge.py
+++ scripts/bridge.py
@@ -9,9 +9,9 @@ class BRIDGE:
         # Create Subscriber to Base Station
         self.sub_deploy = rospy.Subscriber('/'+robot_ns+'/deploy',Bool(),self.deploy_cb)
         # Create Subscriber to Beacon Servos
-        self.sub_servo = rospy.Subscriber('/'+robot_ns+'/servo_status',estop_msgs.ServoStatus(),self.status_cb)
+        self.sub_servo = rospy.Subscriber('/'+robot_ns+'/beacon/solenoid_pos',estop_msgs.ServoStatus(),self.status_cb)
         # Create Publisher to Beacon Drop
-        self.pub_drop = rosppy.Publisher('/'+robot_ns+'/set_relay',estop_msgs.SetChannel.msg,queue_size=10)
+        self.pub_drop = rosppy.Publisher('/'+robot_ns+'/beacon/release_beacon',estop_msgs.SetChannel(),queue_size=10)
         self.store_servo_info = [0,0,0,0,0,0,0,0]
 
     def status_cb(self,msg):
@@ -20,13 +20,13 @@ class BRIDGE:
     def deploy_cb(self,msg):
         drop = estop_msgs.SetChannel()
         drop.state = 1
-        if self.store_servo_info[0] >= 750:
+        if self.store_servo_info[0] >= 0 and self.store_servo_info[0] <= 100:
             drop.id = 0
             pub_drop(drop)
-        elif self.store_servo_info[1] >= 750:
+        elif self.store_servo_info[1] >= 0 and self.store_servo_info[1] <= 100:
             drop.id = 1
             pub_drop(drop)
-        elif self.store_servo_info[2] >= 750:
+        elif self.store_servo_info[2] >= 0 and self.store_servo_info[2] <= 100:
             drop.id = 2
             pub_drop(drop)
         else: 
